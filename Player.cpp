c1b6194ebe7160d160b65b829f6dce4a9035106a
mandk1 <mandk1@mcmaster.ca>
2023-11-27 21:43:19 -0500

    mandk1-iteration 1/2A

--- Player.cpp
+++ Player.cpp
@@ -7,26 +7,99 @@ Player::Player(GameMechs* thisGMRef)
     myDir = STOP;
 
     // more actions to be included
+
+    playerPos.setObjPos(mainGameMechsRef->getBoardSizeX() / 2, mainGameMechsRef->getBoardSizeY() / 2, '*');
+
 }
 
 
 Player::~Player()
 {
     // delete any heap members here
+    //      no heap member yet, leave empty for now
+
 }
 
 void Player::getPlayerPos(objPos &returnPos)
 {
     // return the reference to the playerPos arrray list
+    returnPos.setObjPos(playerPos.x, playerPos.y, playerPos.symbol);
+
 }
 
 void Player::updatePlayerDir()
 {
-    // PPA3 input processing logic        
+    // PPA3 input processing logic   
+    
+    // How to get input!?!?
+    
+    char input = mainGameMechsRef->getInput();
+
+    switch(input)
+    {   
+        case 'w':
+            if (myDir == STOP || myDir == LEFT || myDir == RIGHT) {
+                myDir = UP;
+            }
+            break;
+
+        case 'a':
+            if (myDir == STOP || myDir == UP || myDir == DOWN) {
+                myDir = LEFT;
+            }
+            break;
+
+        case 's':
+            if (myDir == STOP || myDir == LEFT || myDir == RIGHT) {
+                myDir = DOWN;
+            }
+            break;
+
+        case 'd':
+            if (myDir == STOP || myDir == UP || myDir == DOWN) {
+                myDir = RIGHT;
+            }
+            break;
+        default:
+            break;
+
+
+    }
+
 }
 
 void Player::movePlayer()
 {
     // PPA3 Finite State Machine logic
+    switch(myDir)
+    {
+        case UP:
+            plr.y --;
+            if (plr.y < 1){
+                plr.y = mainGameMechsRef->getBoardSizeY() - 1;
+            }
+            break;
+
+        case DOWN:
+            plr.y ++;
+            if (plr.y > mainGameMechsRef->getBoardSizeY() - 1){
+                plr.y = 1;
+            }
+            break;
+
+        case LEFT:
+            plr.x--;
+            if (plr.x < 1){
+                plr.x = mainGameMechsRef->getBoardSizeX() - 1;
+            }
+            break;
+
+        case RIGHT:
+            plr.x++;
+            if(plr.x > mainGameMechsRef->getBoardSizeX() - 1){
+                plr.x = 1;
+            }
+            break;
+    }
 }
 
--- objPosArrayList_TestSuite/objPosArrayList.cpp
+++ objPosArrayList_TestSuite/objPosArrayList.cpp
@@ -5,50 +5,64 @@
 
 objPosArrayList::objPosArrayList()
 {
-
+    aList = new objPos[ARRAY_MAX_CAP];
+    sizeList = 0;
+    sizeArray = ARRAY_MAX_CAP;
 }
 
 objPosArrayList::~objPosArrayList()
 {
-
+    delete[] aList;
 }
 
 int objPosArrayList::getSize()
 {
-
+    return sizeList;
 }
 
 void objPosArrayList::insertHead(objPos thisPos)
 {
-
+    // check if list is full?
+    for (int i = sizeList; i > 0; i--){
+        aList[i] = aList[i-1];
+    }
+    aList[0] = thisPos;
+    sizeList++;
 }
 
 void objPosArrayList::insertTail(objPos thisPos)
 {
-
+    // check?
+    
+    aList[sizeList++] = thisPos;
 }
 
 void objPosArrayList::removeHead()
 {
-    
+    for(int i = 0; i < sizeList - 1; i++){   
+        aList[i] = aList[i + 1];  // shuffling towards the head
+    }
+    sizeList--; // last index of list ready to be overwritten
 }
 
 void objPosArrayList::removeTail()
 {
-
+    sizeList--;  // overwrite last element
 }
 
 void objPosArrayList::getHeadElement(objPos &returnPos)
 {
-
+    returnPos = aList[0];
 }
 
 void objPosArrayList::getTailElement(objPos &returnPos)
 {
-
+    returnPos = aList[sizeList - 1];
 }
 
 void objPosArrayList::getElement(objPos &returnPos, int index)
 {
+    // error checking?
 
+    returnPos = aList[index];
 }
